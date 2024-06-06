5d278f4055e9767d738bcebc4a807ead8521441b
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2024-06-06 12:12:40 -0500

    Fix: Fixing model route

--- pdg-backend/services/model_prediction.py
+++ pdg-backend/services/model_prediction.py
@@ -1,11 +1,17 @@
 import joblib
 import pandas as pd
+import os
 
 def predict_random_forest_model(input_data: dict) -> dict:
   
     data_df = pd.DataFrame(input_data)
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(current_dir, '..\\mlModels\\rf_model.pkl')
+    
+    print(f"Model path: {model_path}")
     # Load the model
-    model = joblib.load('pdg-backend\\mlModels\\xgb_clf_model.pkl')
+    model = joblib.load(model_path)
 
     # Make prediction
     prediction = model.predict(data_df)
@@ -15,10 +21,13 @@ def predict_random_forest_model(input_data: dict) -> dict:
 
 def predict_xgboost_model(input_data: dict) -> dict:
   
-    data_df = pd.DataFrame(input_data, index=[1])
+    data_df = pd.DataFrame(input_data)
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(current_dir, '..\\mlModels\\xgb_clf_model.pkl')
   
     # Load the model
-    model = joblib.load('pdg-backend/aiModels/xgb_clf_model.pkl')
+    model = joblib.load(model_path)
 
     # Make prediction
     prediction = model.predict(data_df)
