ec63685b642fe349a17a74aa2d01fd882f89a6eb
Johan Stiven Ricardo <johan.ricardo1104@gmail.com>
2024-06-08 10:22:25 -0500

    feat: docker file for backend and docker compose for deployment

--- pdg-backend/services/model_prediction.py
+++ pdg-backend/services/model_prediction.py
@@ -7,7 +7,7 @@ def predict_random_forest_model(input_data: dict) -> any:
     data_df = pd.DataFrame([input_data], index=[0])
     
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    model_path = os.path.join(current_dir, '..\\mlModels\\rf_model.pkl')
+    model_path = os.path.join(current_dir, '../mlModels/rf_model.pkl')
     
     # Load the model
     model = joblib.load(model_path)
@@ -23,7 +23,7 @@ def predict_xgboost_model(input_data: dict) -> any:
     data_df = pd.DataFrame([input_data], index=[0])
     
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    model_path = os.path.join(current_dir, '..\\mlModels\\xgboost_model.pkl')
+    model_path = os.path.join(current_dir, '../mlModels/xgboost_model.pkl')
   
     # Load the model
     model = joblib.load(model_path)
